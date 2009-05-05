2d53c2cd4a16464ec93dcf2ce8b0e76b0d2aff3b
Yasuhiro.Matsuda <Yasuhiro.Matsuda@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-05-05 23:55:51 +0000

    changed ListMerger to use PriorityQueue

--- bobo-browse/src/com/browseengine/bobo/util/ListMerger.java
+++ bobo-browse/src/com/browseengine/bobo/util/ListMerger.java
@@ -3,85 +3,146 @@ package com.browseengine.bobo.util;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.TreeMap;
+import java.util.List;
 
+import org.apache.lucene.util.PriorityQueue;
+
+/**
+ * @author ymatsuda
+ *
+ */
 public class ListMerger
 {
-  private ListMerger()
+  public static class MergedIterator<T> implements Iterator<T>
   {
-    
-  }
-  
-  public static <T> Iterator<T> mergeLists(final Iterator<T>[] iterators,final Comparator<T> comparator){
-    
-    return new Iterator<T>(){
-        TreeMap<T,Iterator<T>> map=new TreeMap<T,Iterator<T>>(comparator);
-        {
-            for (Iterator<T> result : iterators)
-            {
-                if (result.hasNext())
-                {
-                    map.put(result.next(),result);
-                }
-            }
-        }
-        
-        public boolean hasNext() {
-            return map.size()>0;
-        }
-
-        public T next() {
-            T first=map.firstKey();
-            Iterator<T> iter=map.remove(first);
-            while (iter.hasNext())
-            {
-                T next=iter.next();
-                if (!map.containsKey(next))
-                {
-                    map.put(next,iter);
-                    break;
-                }
-            }
-            return first;
-        }
-
-        public void remove() {
-            T first=map.firstKey();
-            Iterator<T> iter=map.remove(first);
-            while (iter.hasNext())
-            {
-                T next=iter.next();
-                if (!map.containsKey(next))
-                {
-                    map.put(next,iter);
-                    break;
-                }
-            }
-        }
-    };
-}
-  
-  public static <T> ArrayList<T> mergeLists(int offset,int count,Iterator<T>[] iterators,Comparator<T> comparator)
-  {
-    Iterator<T> mergedIter=mergeLists(iterators, comparator);
-    
-    int c=0;
-    while(c<offset && mergedIter.hasNext())
+    private class IteratorNode
     {
-      mergedIter.next();
-      c++;
+      public Iterator<T> _iterator;
+      public T _curVal;
+      
+      public IteratorNode(Iterator<T> iterator)
+      {
+        _iterator = iterator;
+        _curVal = null;
+      }
+        
+      public boolean fetch()
+      {
+        if(_iterator.hasNext())
+        {
+          _curVal = _iterator.next();
+          return true;
+        }
+        _curVal = null;
+        return false;
+      }
+    }
+
+    private final PriorityQueue _queue;
+
+    private MergedIterator(final int length, final Comparator<T> comparator)
+    {
+      _queue = new PriorityQueue()
+      {
+        {
+          this.initialize(length);
+        }
+      
+        @SuppressWarnings("unchecked")
+        @Override
+        protected boolean lessThan(Object o1, Object o2)
+        {
+          T v1 = ((IteratorNode)o1)._curVal;
+          T v2 = ((IteratorNode)o2)._curVal;
+          
+          return (comparator.compare(v1, v2) < 0);
+        }
+      };
     }
     
-    ArrayList<T> mergedList=new ArrayList<T>();
+    public MergedIterator(final List<Iterator<T>> iterators, final Comparator<T> comparator)
+    {
+      this(iterators.size(), comparator);
+      for(Iterator<T> iterator : iterators)
+      {
+        IteratorNode ctx = new IteratorNode(iterator);
+        if(ctx.fetch()) _queue.insert(ctx);
+      }
+    }
     
-    c=0;
-    while(mergedIter.hasNext() && c<count)
+    public MergedIterator(final Iterator<T>[] iterators, final Comparator<T> comparator)
+    {
+      this(iterators.length, comparator);
+      for(Iterator<T> iterator : iterators)
+      {
+        IteratorNode ctx = new IteratorNode(iterator);
+        if(ctx.fetch()) _queue.insert(ctx);
+      }
+    }
+
+    public boolean hasNext()
+    {
+      return _queue.size() > 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T next()
+    {
+      IteratorNode ctx = (IteratorNode)_queue.top();
+      T val = ctx._curVal;
+      if (ctx.fetch())
+      {
+        _queue.adjustTop();
+      }
+      else
+      {
+        _queue.pop();
+      }
+      return val;
+    }
+    
+    public void remove()
+    {
+      throw new UnsupportedOperationException();
+    }
+  }
+  
+  private ListMerger() { }
+ 
+  public static <T> Iterator<T> mergeLists(final Iterator<T>[] iterators, final Comparator<T> comparator)
+  {
+    return new MergedIterator<T>(iterators, comparator);
+  }
+  
+  public static <T> Iterator<T> mergeLists(final List<Iterator<T>> iterators, final Comparator<T> comparator)
+  {
+    return new MergedIterator<T>(iterators, comparator);
+  }
+  
+  public static <T> ArrayList<T> mergeLists(int offset, int count, Iterator<T>[] iterators, Comparator<T> comparator)
+  {
+    return mergeLists(offset, count, new MergedIterator<T>(iterators, comparator));
+  }
+  
+  public static <T> ArrayList<T> mergeLists(int offset, int count, List<Iterator<T>> iterators, Comparator<T> comparator)
+  {
+    return mergeLists(offset, count, new MergedIterator<T>(iterators, comparator));
+  }
+  
+  private static <T> ArrayList<T> mergeLists(int offset, int count, Iterator<T> mergedIter)
+  {
+    for (int c = 0; c < offset && mergedIter.hasNext(); c++)
+    {
+      mergedIter.next();
+    }
+    
+    ArrayList<T> mergedList = new ArrayList<T>();
+    
+    for (int c = 0; c < count && mergedIter.hasNext(); c++)
     {
       mergedList.add(mergedIter.next());
-      c++;
     }
     
     return mergedList;
-    
   }
 }
