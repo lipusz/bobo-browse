79a720bf754f02984f871008fddbf2615f735dad
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-07-20 20:38:34 +0000

    removing obsolete branch

--- branches/BR_PERF_IMPROVEMENT/bobo-service/src/com/browseengine/bobo/service/BoboBrowseService.java
+++
@@ -1,9 +0,0 @@
-package com.browseengine.bobo.service;
-
-import com.browseengine.bobo.api.BrowseException;
-import com.browseengine.bobo.api.BrowseRequest;
-import com.browseengine.bobo.api.BrowseResult;
-
-public interface BoboBrowseService {
-	BrowseResult browse(BrowseRequest req) throws BrowseException;
-}
