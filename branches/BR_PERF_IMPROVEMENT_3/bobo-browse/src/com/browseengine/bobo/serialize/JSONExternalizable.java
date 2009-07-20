381ce5ec29422c7f1255e75914be5a25288d705e
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-07-20 20:37:55 +0000

    removing obsolete branch

--- branches/BR_PERF_IMPROVEMENT_3/bobo-browse/src/com/browseengine/bobo/serialize/JSONExternalizable.java
+++
@@ -1,8 +0,0 @@
-package com.browseengine.bobo.serialize;
-
-import org.json.JSONObject;
-
-public interface JSONExternalizable extends JSONSerializable {
-	JSONObject toJSON() throws JSONSerializationException;
-	void fromJSON(JSONObject obj) throws JSONSerializationException;
-}
