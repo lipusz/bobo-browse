3d963c2f2358cfcf5e399a3ede6c054d41cb19df
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-07-20 20:38:25 +0000

    removing obsolete branch

--- branches/BR_PERF_IMPROVEMENT_2/bobo-browse/src/com/browseengine/bobo/util/Collector.java
+++
@@ -1,9 +0,0 @@
-package com.browseengine.bobo.util;
-
-public interface Collector<T>
-{
-	/** return true if the collection grows
-		@param bskip causes the collector to make a "dry run"
-	 */
-	public boolean collect(T obj, boolean bskip);
-}
