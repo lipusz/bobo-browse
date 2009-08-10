f4206d6bfae3f80526efe9f4c1606ccb76d11c25
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-08-10 16:28:29 +0000

    merged from BR_SOLR_PLUGIN_2@183:194

--- bobo-browse/src/com/browseengine/bobo/service/BrowseHitConverter.java
+++ bobo-browse/src/com/browseengine/bobo/service/BrowseHitConverter.java
@@ -19,7 +19,7 @@ public class BrowseHitConverter implements Converter {
 
 	public void marshal(Object obj, HierarchicalStreamWriter writer,
 			MarshallingContext ctx) {
-		
+		writer.startNode("hit");
 		BrowseHit hit=(BrowseHit)obj;
 		writer.addAttribute("score", String.valueOf(hit.getScore()));
 		writer.addAttribute("docid", String.valueOf(hit.getDocid()));
@@ -39,12 +39,13 @@ public class BrowseHitConverter implements Converter {
 				writer.endNode();
 			}
 		}
+		writer.endNode();
 	}
 
 	public Object unmarshal(HierarchicalStreamReader reader,
 			UnmarshallingContext ctx) {
 		BrowseHit hit=new BrowseHit();
-		
+		reader.moveDown();
 		String scoreString=reader.getAttribute("score");
 		if (scoreString!=null){
 			hit.setScore(Float.parseFloat(scoreString));
@@ -55,7 +56,7 @@ public class BrowseHitConverter implements Converter {
 		}
 		
 		int numFields=0;
-		String fieldCountString=reader.getAttribute("numFields");
+		String fieldCountString=reader.getAttribute("numfields");
 		if (fieldCountString!=null){
 			numFields=Integer.parseInt(fieldCountString);
 		}
@@ -64,16 +65,14 @@ public class BrowseHitConverter implements Converter {
 		hit.setFieldValues(fieldVals);
 		for (int i=0;i<numFields;++i){
 			reader.moveDown();
-			String nodeName=reader.getNodeName();
-			if ("field".equals(nodeName)){
-				String fieldname=reader.getAttribute("name");
-				String fieldval=reader.getValue();
-				String s2=fieldval.substring(1,fieldval.length()-1);
-				String[] parts=s2.split(", ");
-				fieldVals.put(fieldname, parts);
-			}
+			String fieldname=reader.getNodeName();
+			String fieldval = reader.getValue();
+			String s2=fieldval.substring(1,fieldval.length()-1);
+			String[] parts=s2.split(", ");
+			fieldVals.put(fieldname, parts);
 			reader.moveUp();
 		}
+		reader.moveUp();
 		return hit;
 	}
 
--- bobo-browse/src/com/browseengine/bobo/service/BrowseResultConverter.java
+++ bobo-browse/src/com/browseengine/bobo/service/BrowseResultConverter.java
@@ -1,6 +1,8 @@
 package com.browseengine.bobo.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
@@ -8,6 +10,7 @@ import com.browseengine.bobo.api.BrowseFacet;
 import com.browseengine.bobo.api.BrowseHit;
 import com.browseengine.bobo.api.BrowseResult;
 import com.browseengine.bobo.api.FacetAccessible;
+import com.browseengine.bobo.api.MappedFacetAccessible;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
@@ -40,7 +43,7 @@ public class BrowseResultConverter implements Converter {
 			writer.addAttribute("facetcount", String.valueOf(facetList.size()));
 			
 			for (BrowseFacet facet : facetList){
-				writer.startNode("facet");
+				writer.startNode("facet-value");
 				writer.addAttribute("value", String.valueOf(facet.getValue()));
 				writer.addAttribute("count", String.valueOf(facet.getHitCount()));
 				writer.endNode();
@@ -50,7 +53,13 @@ public class BrowseResultConverter implements Converter {
 		writer.endNode();
 		writer.startNode("hits");
 		BrowseHit[] hits=result.getHits();
-		ctx.convertAnother(hits);
+
+		writer.addAttribute("length", String.valueOf(hits==null ? 0 : hits.length));
+		
+		for (BrowseHit hit : hits){
+			ctx.convertAnother(hit);
+		}
+		
 		writer.endNode();
 	}
 
@@ -58,10 +67,68 @@ public class BrowseResultConverter implements Converter {
 			UnmarshallingContext ctx) {		
 		BrowseResult res=new BrowseResult();
 		
+		String numHitsString=reader.getAttribute("numhits");
+		if (numHitsString!=null){
+			res.setNumHits(Integer.parseInt(numHitsString));
+		}
 		
+		String totalDocsString=reader.getAttribute("totaldocs");
+		if (totalDocsString!=null){
+			res.setTotalDocs(Integer.parseInt(totalDocsString));
+		}
 		
-		BrowseHit[] hits=(BrowseHit[])ctx.convertAnother(res,BrowseHit[].class);
-		res.setHits(hits);
+		String timeString=reader.getAttribute("time");
+		if (timeString!=null){
+			res.setTime(Long.parseLong(timeString));
+		}
+		
+		while (reader.hasMoreChildren()){
+			reader.moveDown();
+			if ("facets".equals(reader.getNodeName())){
+				Map<String,FacetAccessible> facetMap = new HashMap<String,FacetAccessible>();
+				String facetCountString = reader.getAttribute("count");
+				if (facetCountString!=null){
+					int count = Integer.parseInt(facetCountString);
+					if (count > 0){
+						for (int i=0;i<count;++i){
+							reader.moveDown();
+							String name = reader.getAttribute("name");
+							String countStr = reader.getAttribute("facetcount");
+							int fcount = 0;
+							if (countStr!=null){
+								fcount = Integer.parseInt(countStr);
+							}
+							BrowseFacet[] facets = new BrowseFacet[fcount];
+							for (int k=0;k<fcount;++k){
+								facets[k]=new BrowseFacet();
+								reader.moveDown();
+								String valueString=reader.getAttribute("value");
+								facets[k].setValue(valueString);
+								
+								String countString=reader.getAttribute("count");
+								if (countString!=null){
+									facets[k].setHitCount(Integer.parseInt(countString));
+								}
+								reader.moveUp();
+							}
+							facetMap.put(name,new MappedFacetAccessible(facets));
+							reader.moveUp();
+						}
+						res.addAll(facetMap);
+					}
+				}
+			}
+			else if ("hits".equals(reader.getNodeName())){
+				String countStr = reader.getAttribute("length");
+				int hitLen = Integer.parseInt(countStr);
+				BrowseHit[] hits = new BrowseHit[hitLen];
+				for (int i = 0; i< hitLen; ++i){
+					hits[i]=(BrowseHit)ctx.convertAnother(res, BrowseHit.class);
+				}
+				res.setHits(hits);
+			}
+			reader.moveUp();
+		}
 		return res;
 	}
 
