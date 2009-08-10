2f81360be0e3d4e9dc1949aabcc60e2037640a64
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-08-10 16:28:29 +0000

    merged from BR_SOLR_PLUGIN_2@183:194

--- trunk/bobo-solr/src/com/browseengine/solr/BoboRequestHandler.java
+++ trunk/bobo-solr/src/com/browseengine/solr/BoboRequestHandler.java
@@ -2,6 +2,8 @@ package com.browseengine.solr;
 
 import java.net.URL;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.search.Query;
@@ -31,10 +33,17 @@ public class BoboRequestHandler implements SolrRequestHandler {
 	private static final String NAME="Bobo-Browse";
 	
 	static final String BOBORESULT="boboresult";
+	public static final String SHARD_PARAM = "shards";
+	
+	private static final String THREAD_POOL_SIZE_PARAM = "thread_pool_size";
+
+	private static final String MAX_SHARD_COUNT_PARAM = "max_shard_count";
+	
+    private ExecutorService _threadPool = null;
 	
 	private static Logger logger=Logger.getLogger(BoboRequestHandler.class);
 	
-	private static class BoboSolrParams extends BoboParams{
+	public static class BoboSolrParams extends BoboParams{
 		SolrParams _params;
 		BoboSolrParams(SolrParams params){
 			_params=params;
@@ -55,11 +64,9 @@ public class BoboRequestHandler implements SolrRequestHandler {
         {
           return _params.getParams(name);
         }
-		
-		
 	}
 	
-	private static class BoboSolrQueryBuilder extends BoboQueryBuilder{
+	public static class BoboSolrQueryBuilder extends BoboQueryBuilder{
 		SolrQueryRequest _req;
 		BoboSolrQueryBuilder(SolrQueryRequest req){
 			_req=req;
@@ -82,43 +89,72 @@ public class BoboRequestHandler implements SolrRequestHandler {
 	
 	public void handleRequest(SolrQueryRequest req, SolrQueryResponse rsp) {
 		
-		SolrIndexSearcher searcher=req.getSearcher();
+		BoboSolrParams boboSolrParams = new BoboSolrParams(req.getParams());
 		
-		SolrIndexReader solrReader = searcher.getReader();
-		BoboIndexReader reader = (BoboIndexReader)solrReader.getWrappedReader();
+		String shardsVal = boboSolrParams.getString(SHARD_PARAM, null);
+		BrowseRequest br=BoboRequestBuilder.buildRequest(boboSolrParams,new BoboSolrQueryBuilder(req));
+		logger.info("browse request: "+br);
 		
-		if (reader instanceof BoboIndexReader){
-			BrowseRequest br=BoboRequestBuilder.buildRequest(new BoboSolrParams(req.getParams()),new BoboSolrQueryBuilder(req));
-			logger.info("browse request: "+br);
+		BrowseResult res = null;
+		if (shardsVal == null)
+		{
+			SolrIndexSearcher searcher=req.getSearcher();
 			
-			BoboBrowser browser = new BoboBrowser(reader);
-		    
-		    try {
-				BrowseResult res=browser.browse(br);
-				rsp.add(BOBORESULT, res);
-				 /*
-				if(HighlightingUtils.isHighlightingEnabled(req) && query != null) {
-					NamedList sumData = HighlightingUtils.doHighlighting(
-					        results.docList, query.rewrite(req.getSearcher().getReader()), req, new String[]{defaultField});
-					      if(sumData != null)
-					        rsp.add("highlighting", sumData);
+			SolrIndexReader solrReader = searcher.getReader();
+			BoboIndexReader reader = (BoboIndexReader)solrReader.getWrappedReader();
+			
+			if (reader instanceof BoboIndexReader){
+				BoboBrowser browser = new BoboBrowser(reader);
+			    
+			    try {
+					res=browser.browse(br);
+					 /*
+					if(HighlightingUtils.isHighlightingEnabled(req) && query != null) {
+						NamedList sumData = HighlightingUtils.doHighlighting(
+						        results.docList, query.rewrite(req.getSearcher().getReader()), req, new String[]{defaultField});
+						      if(sumData != null)
+						        rsp.add("highlighting", sumData);
+					}
+					*/
+					
+				} catch (BrowseException e) {
+					logger.error(e.getMessage(),e);
+					throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,e.getMessage(),e);
 				}
-				*/
-				
-			} catch (BrowseException e) {
-				logger.error(e.getMessage(),e);
-				throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,e.getMessage(),e);
+			   
 			}
-		   
+			else{
+		        throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"invalid reader, please make sure BoboIndexReaderFactory is set.");
+			}
 		}
 		else{
-	        throw new SolrException(SolrException.ErrorCode.SERVER_ERROR,"invalid reader, please make sure BoboIndexReaderFactory is set.");
+			// multi sharded request
+			String[] shards = shardsVal.split(",");
+			res = DispatchUtil.broadcast(_threadPool, boboSolrParams, br, shards, 5);
 		}
+		rsp.add(BOBORESULT, res);
+		
 	}
+	
 
 	public void init(NamedList params) {
-		// TODO Auto-generated method stub
-
+		int threadPoolSize;
+		try{
+			threadPoolSize = Integer.parseInt((String)params.get(THREAD_POOL_SIZE_PARAM));
+		}
+		catch(Exception e){
+			threadPoolSize = 100;
+		}
+		
+		int shardCount;
+		try{
+			shardCount = Integer.parseInt((String)params.get(MAX_SHARD_COUNT_PARAM));
+		}
+		catch(Exception e){
+			shardCount = 10;
+		}
+		
+		_threadPool = Executors.newFixedThreadPool(threadPoolSize * shardCount);
 	}
 
 	public Category getCategory() {
