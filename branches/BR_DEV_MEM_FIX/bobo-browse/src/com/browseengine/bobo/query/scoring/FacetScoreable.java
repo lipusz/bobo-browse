e7955a86a919d842eeef3e8f1251b74a5481be5c
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-10-24 06:32:36 +0000

    removed dead branch

--- branches/BR_DEV_MEM_FIX/bobo-browse/src/com/browseengine/bobo/query/scoring/FacetScoreable.java
+++
@@ -1,7 +0,0 @@
-package com.browseengine.bobo.query.scoring;
-
-import java.util.Map;
-
-public interface FacetScoreable {
-	 BoboDocScorer getDocScorer(FacetTermScoringFunctionFactory scoringFunctionFactory,Map<String,Float> boostMap);
-}
