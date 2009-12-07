4b679f256ee9fea195b49ed67710277e2b5cc3b4
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2009-12-07 03:48:39 +0000

    enhanced tool

--- bobo-perf/src/com/browseengine/bobo/perf/ExpandIndex.java
+++ bobo-perf/src/com/browseengine/bobo/perf/ExpandIndex.java
@@ -2,29 +2,67 @@ package com.browseengine.bobo.perf;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriter.MaxFieldLength;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 
 public class ExpandIndex {
-  public static void main(String[] args) throws IOException {
-	File srcIdx = new File(args[0]);
-	File targetIndx = new File(args[1]);
-	
-	int times = Integer.parseInt(args[2]);
-	
-	Directory[] multipler = new Directory[times];
-	
-	IndexWriter writer = new IndexWriter(targetIndx,new StandardAnalyzer(),true,MaxFieldLength.UNLIMITED);
-	for (int i = 0;i<times;++i){
-		multipler[i]=FSDirectory.getDirectory(srcIdx);
+	public static void main(String[] args) throws IOException {
+		File srcIdx = new File(args[0]);
+		File targetIndx = new File(args[1]);
+
+		int numReps = Integer.parseInt(args[2]);
+		int numsegs = Integer.parseInt(args[3]);
+
+		Directory srcDir = FSDirectory.getDirectory(srcIdx);
+		Directory targetDir = FSDirectory.getDirectory(targetIndx);
+		int timesReplicatePerSeg = numReps / numsegs;
+		IndexWriter writer = new IndexWriter(targetDir, new StandardAnalyzer(),
+				MaxFieldLength.UNLIMITED);
+		ArrayList<Directory> dirList = new ArrayList<Directory>(numsegs);
+		System.out.println("num segments: " + numsegs);
+		System.out.println("num reps per segment: " + timesReplicatePerSeg);
+		for (int i = 0; i < numsegs; ++i) {
+			RAMDirectory ramDir = new RAMDirectory();
+			IndexWriter subWriter = new IndexWriter(ramDir,
+					new StandardAnalyzer(), MaxFieldLength.UNLIMITED);
+			Directory[] multiplier = new Directory[timesReplicatePerSeg];
+			for (int k = 0; k < timesReplicatePerSeg; ++k) {
+				multiplier[k] = new RAMDirectory(srcDir);
+			}
+			subWriter.addIndexesNoOptimize(multiplier);
+			subWriter.optimize();
+			subWriter.close();
+			dirList.add(ramDir);
+			System.out.println("segment: " + i + " created");
+		}
+
+		writer.setMaxMergeDocs(Integer.MAX_VALUE);
+		writer.setMergeFactor(Integer.MAX_VALUE);
+		System.out.println("building result index");
+		writer.addIndexesNoOptimize(dirList.toArray(new Directory[dirList
+				.size()]));
+		writer.close();
+		
+		File boboSpring = new File(srcIdx,"bobo.spring");
+		if (boboSpring.exists()){
+			String cmd = "cp "+boboSpring.getAbsolutePath()+" "+targetIndx.getAbsolutePath();
+			System.out.println("executing: "+cmd);
+			Runtime.getRuntime().exec(cmd);
+		}
+		
+		/*
+		 * SegmentInfos infos=new SegmentInfos(); infos.read(targetDir);
+		 * System.out.println("result segment count: "+infos.size());
+		 */
+		IndexReader r = IndexReader.open(targetDir, true);
+		System.out.println("num docs: " + r.numDocs());
+		r.close();
 	}
-	writer.addIndexesNoOptimize(multipler);
-	System.out.println("optimizing...");
-	writer.optimize();
-	writer.close();
-  }
 }
