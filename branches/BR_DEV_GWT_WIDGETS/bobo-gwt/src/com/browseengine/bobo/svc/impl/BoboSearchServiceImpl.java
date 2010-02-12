bc963237621dd8f29d014a0685ec0bf871a06671
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2010-02-12 23:44:22 +0000

    hooked up rpc svc with spring

--- branches/BR_DEV_GWT_WIDGETS/bobo-gwt/src/com/browseengine/bobo/svc/impl/BoboSearchServiceImpl.java
+++ branches/BR_DEV_GWT_WIDGETS/bobo-gwt/src/com/browseengine/bobo/svc/impl/BoboSearchServiceImpl.java
@@ -1,18 +1,31 @@
 package com.browseengine.bobo.svc.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.SortField;
 
 import com.browseengine.bobo.api.BrowseException;
+import com.browseengine.bobo.api.BrowseFacet;
+import com.browseengine.bobo.api.BrowseHit;
 import com.browseengine.bobo.api.BrowseRequest;
 import com.browseengine.bobo.api.BrowseResult;
+import com.browseengine.bobo.api.FacetAccessible;
+import com.browseengine.bobo.api.FacetSpec;
+import com.browseengine.bobo.api.FacetSpec.FacetSortSpec;
+import com.browseengine.bobo.gwt.svc.BoboFacetSpec;
+import com.browseengine.bobo.gwt.svc.BoboHit;
 import com.browseengine.bobo.gwt.svc.BoboRequest;
 import com.browseengine.bobo.gwt.svc.BoboResult;
 import com.browseengine.bobo.gwt.svc.BoboSearchService;
 import com.browseengine.bobo.gwt.svc.BoboSortSpec;
+import com.browseengine.bobo.gwt.widgets.FacetValue;
 import com.browseengine.bobo.service.BrowseService;
 
 public class BoboSearchServiceImpl implements BoboSearchService {
@@ -23,6 +36,16 @@ public class BoboSearchServiceImpl implements BoboSearchService {
 	public BoboSearchServiceImpl(BrowseService svc,QueryParser qparser){
 		_svc = svc;
 		_qparser = qparser;
+	}
+	
+	private static FacetSpec convert(BoboFacetSpec spec){
+		FacetSpec fspec = new FacetSpec();
+		
+		fspec.setMaxCount(spec.getMax());
+		fspec.setMinHitCount(spec.getMinCount());
+		fspec.setExpandSelection(spec.isExpandSelection());
+		fspec.setOrderBy(spec.isOrderByHits() ? FacetSortSpec.OrderHitsDesc : FacetSortSpec.OrderValueAsc);
+		return fspec;
 	}
 	
 	private BrowseRequest convert(BoboRequest req) throws Exception{
@@ -44,11 +67,53 @@ public class BoboSearchServiceImpl implements BoboSearchService {
 		  }
 		  breq.setSort(sorts);
 		}
+		
+		Map<String,BoboFacetSpec> facetMap = req.getFacetSpecMap();
+		if (facetMap!=null && facetMap.size()>0){
+			Set<Entry<String,BoboFacetSpec>> entrySet = facetMap.entrySet();
+			for (Entry<String,BoboFacetSpec> entry : entrySet){
+				breq.setFacetSpec(entry.getKey(), convert(entry.getValue()));
+			}
+		}
 		return breq;
 	}
 	
+	private static BoboHit convert(BrowseHit hit){
+		BoboHit bhit = new BoboHit();
+		bhit.setDocid(hit.getDocid());
+		bhit.setScore(hit.getScore());
+		bhit.setFields(hit.getFieldValues());
+		return bhit;
+	}
+	
 	private BoboResult convert(BrowseResult res) throws Exception{
-		return null;
+		BoboResult bres = new BoboResult();
+		bres.setNumHits(res.getNumHits());
+		bres.setTotalDocs(res.getTotalDocs());
+		bres.setTime(res.getTime());
+		BrowseHit[] bhits = res.getHits();
+		List<BoboHit> hits = new ArrayList<BoboHit>(bhits.length);
+		for (BrowseHit bhit : bhits){
+			hits.add(convert(bhit));
+		}
+		Map<String,FacetAccessible> facetMap = res.getFacetMap();
+		if (facetMap!=null && facetMap.size()>0){
+			Map<String,List<FacetValue>> fmap = new HashMap<String,List<FacetValue>>();
+			Set<Entry<String,FacetAccessible>> entrySet = facetMap.entrySet();
+			for (Entry<String,FacetAccessible> entry : entrySet){
+				FacetAccessible accessor = entry.getValue();
+				List<BrowseFacet> facetList = accessor.getFacets();
+				ArrayList<FacetValue> facetValList = new ArrayList<FacetValue>(facetList.size());
+				for (BrowseFacet bfacet : facetList){
+					FacetValue fval = new FacetValue(bfacet.getValue(),bfacet.getHitCount());
+					facetValList.add(fval);
+				}
+				fmap.put(entry.getKey(), facetValList);
+			}
+			bres.setFacetResults(fmap);
+		}
+		bres.setHits(hits);
+		return bres;
 	}
 	
 	public BoboResult search(BoboRequest req) {
