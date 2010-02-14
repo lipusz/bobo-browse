48aee6d6e0b54445f37fe4e8ca52e9367d481548
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2010-02-14 09:07:41 +0000

    widget clean up

--- branches/BR_DEV_GWT_WIDGETS/bobo-demo/src/com/browseengine/bobo/demo/client/BoboDemoPanel.java
+++ branches/BR_DEV_GWT_WIDGETS/bobo-demo/src/com/browseengine/bobo/demo/client/BoboDemoPanel.java
@@ -1,59 +1,141 @@
 package com.browseengine.bobo.demo.client;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
+import com.browseengine.bobo.gwt.svc.BoboFacetSpec;
+import com.browseengine.bobo.gwt.svc.BoboRequest;
+import com.browseengine.bobo.gwt.svc.BoboResult;
 import com.browseengine.bobo.gwt.svc.BoboSearchServiceAsync;
+import com.browseengine.bobo.gwt.svc.BoboSelection;
 import com.browseengine.bobo.gwt.widgets.AbstractFacetView;
 import com.browseengine.bobo.gwt.widgets.CheckBoxFacetView;
 import com.browseengine.bobo.gwt.widgets.FacetSelectionListener;
 import com.browseengine.bobo.gwt.widgets.FacetValue;
 import com.browseengine.bobo.gwt.widgets.FacetValueSelectionEvent;
 import com.browseengine.bobo.gwt.widgets.TagCloudFacetView;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class BoboDemoPanel extends Composite implements FacetSelectionListener{
-  //  interface MyUiBinder extends UiBinder<Widget, BoboDemoPanel> {}
-  //  private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
-    private final BoboSearchServiceAsync  _searchSvc;
-	
+    interface MyUiBinder extends UiBinder<Widget, BoboDemoPanel> {}
+    private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+    private final BoboSearchServiceAsync  _searchSvc;	
+    private final BoboRequest _req;
+    
+    private final Map<String,AbstractFacetView> _viewMap;
+
+    @UiField CheckBoxFacetView colorView;
+    @UiField TagCloudFacetView tagsView;
+    
     public BoboDemoPanel(BoboSearchServiceAsync searchSvc) {
     	_searchSvc = searchSvc;
-    //	initWidget(uiBinder.createAndBindUi(this));
+    	_req = new BoboRequest();
+    	Map<String,BoboFacetSpec> facetSpecMap = new HashMap<String,BoboFacetSpec>();
+    	BoboFacetSpec colorSpec = new BoboFacetSpec();
+    	colorSpec.setMax(10);
+    	colorSpec.setExpandSelection(true);
+    	colorSpec.setOrderByHits(true);
     	
-    	CheckBoxFacetView checkbox = new CheckBoxFacetView("color");
-        checkbox.addFacetSelectionListener(this);
-        FacetValue[] facets = new FacetValue[]{new FacetValue("red",1234),new FacetValue("green",510)};
-        //String[] facets = new String[]{"red","green"};
-        List<FacetValue> list = Arrays.asList(facets);
-        checkbox.updateSelections(list,null);
-        Label label = new Label();
-        label.setText("test");
-        VerticalPanel panel = new VerticalPanel();
-        panel.add(label);
-        panel.add(checkbox);
-        
-        TagCloudFacetView TagCloudFacetView = new TagCloudFacetView("color");
-        TagCloudFacetView.addFacetSelectionListener(this);
-        TagCloudFacetView.updateSelections(list,null);
-        panel.add(TagCloudFacetView);
-        initWidget(panel);
+    	
+    	initWidget(uiBinder.createAndBindUi(this));
+    	
+    	colorView.addFacetSelectionListener(this);
+    	facetSpecMap.put(colorView.getName(), colorSpec);
+    	_req.setFacetSpecMap(facetSpecMap);
+    	
+    	_viewMap = new HashMap<String,AbstractFacetView>();
+    	_viewMap.put(colorView.getName(), colorView);
+    	
+       // tagsView.addFacetSelectionListener(this);
+       // tagsView.updateSelections(list,null);
+        executeSearch();
 	}
     
     
 
     public void handleSelectedEvent(FacetValueSelectionEvent event) {
-		Window.alert(event.getSource().getName()+":"+event.getFacetValue()+" -> selected");
+    	Map<String,BoboSelection> selMap = _req.getSelections();
+    	
+		if (selMap==null){
+			selMap=new HashMap<String,BoboSelection>();
+			_req.setSelections(selMap);
+		}
+		
+		AbstractFacetView view = event.getSource();
+    	String name = view.getName();
+    	
+		BoboSelection sel = selMap.get(name);
+		if (sel==null){
+			sel = new BoboSelection();
+			selMap.put(name, sel);
+		}
+		sel.setValues(Arrays.asList(event.getFacetValue()));
+		
+		executeSearch();
 	}
 	
 	public void handleUnSelectedEvent(FacetValueSelectionEvent event) {
-		Window.alert(event.getSource().getName()+":"+event.getFacetValue()+" -> unselected");
+		Map<String,BoboSelection> selMap = _req.getSelections();
+    	AbstractFacetView view = event.getSource();
+    	String name = view.getName();
+		if (selMap!=null){
+			BoboSelection sel = selMap.get(name);
+			if (sel!=null){
+				List<String> vals = sel.getValues();
+				if (vals!=null && vals.size()>0){
+				  vals.remove(event.getFacetValue());
+				  executeSearch();
+				}
+			}
+		}
+		
 	}
 	  
 	public void handleClearSelections(AbstractFacetView view){
-		Window.alert("selections cleared: "+view.getName());
+		Map<String,BoboSelection> selMap = _req.getSelections();
+		if (selMap!=null){
+			selMap.remove(view.getName());
+		}
+		executeSearch();
+	}
+	
+	void refreshViews(BoboResult res){
+		Map<String,List<FacetValue>> valMap = res.getFacetResults();
+		Set<Entry<String,List<FacetValue>>> entrySet = valMap.entrySet();
+		for (Entry<String,List<FacetValue>> entry : entrySet){
+			String facetName = entry.getKey();
+			AbstractFacetView view = _viewMap.get(facetName);
+			if (view!=null){
+			  view.updateSelections(entry.getValue(), null);
+			}
+		}
+	}
+	
+	private void executeSearch(){
+		_searchSvc.search(_req, new AsyncCallback<BoboResult>() {
+			
+			public void onSuccess(BoboResult result) {
+				if (result!=null){
+					refreshViews(result);
+				}
+				else{
+					Window.alert("null result!");
+				}
+			}
+			
+			public void onFailure(Throwable caught) {
+				Window.alert("server error: "+caught.getMessage());
+			}
+		});
 	}
 }
