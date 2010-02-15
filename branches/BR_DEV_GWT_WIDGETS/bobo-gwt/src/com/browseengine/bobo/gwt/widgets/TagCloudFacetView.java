c54c52f1878899fbe933e920dfc6b89eaca6d7fc
john.wang <john.wang@aa625b76-1ba8-11de-8b5c-13accb87f508>
2010-02-15 08:43:40 +0000

    bug fixes and added multi selection

--- branches/BR_DEV_GWT_WIDGETS/bobo-gwt/src/com/browseengine/bobo/gwt/widgets/TagCloudFacetView.java
+++ branches/BR_DEV_GWT_WIDGETS/bobo-gwt/src/com/browseengine/bobo/gwt/widgets/TagCloudFacetView.java
@@ -3,7 +3,6 @@ package com.browseengine.bobo.gwt.widgets;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -40,30 +39,13 @@ public class TagCloudFacetView extends AbstractFacetView {
 	}
 	
 	@Override
-	public void updateSelections(List<FacetValue> selections,Set<String> selected) {
+	public void updateSelections(List<FacetValue> selections) {
 		_selTagsPane.clear();
-		if (selected!=null && selected.size()>0){
-			for (String sel : selected){
-			  Anchor anchor = new Anchor();
-			  _selTagsPane.add(anchor);
-			  anchor.setText(sel);
-			  anchor.setStyleName(getStyleName());
-			  anchor.addClickHandler(new ClickHandler(){
-
-				public void onClick(ClickEvent event) {
-					Anchor src = (Anchor)event.getSource();
-					fireFacetSelectionEvent(src.getText(),false);
-				}
-				  
-			  });
-			}
-		}
-		
 		_mainPane.clear();
 		int totalCount = 0;
 		if (selections!=null){
 			for (FacetValue facet : selections){
-				totalCount+=facet.getHitcount();
+				totalCount+=facet.getCount();
 			}
 			Collections.sort(selections, new Comparator<FacetValue>(){
 				public int compare(FacetValue o1, FacetValue o2) {
@@ -72,8 +54,8 @@ public class TagCloudFacetView extends AbstractFacetView {
 			});
 			for (FacetValue facet : selections){
 				String val = facet.getValue();
-				if (selected==null || !selected.contains(val)){
-				  int size = facet.getHitcount()*10/totalCount;
+				if (!facet.isSelected()){
+				  int size = facet.getCount()*10/totalCount;
 				  Anchor anchor = new Anchor();
 				  Label label = new Label(" ");
 				  _mainPane.add(label);
@@ -87,6 +69,20 @@ public class TagCloudFacetView extends AbstractFacetView {
 						fireFacetSelectionEvent(src.getText(),true);
 					}
 				  });
+				}
+				else{
+					Anchor anchor = new Anchor();
+					  _selTagsPane.add(anchor);
+					  anchor.setText(val);
+					  anchor.setStyleName(getStyleName());
+					  anchor.addClickHandler(new ClickHandler(){
+
+						public void onClick(ClickEvent event) {
+							Anchor src = (Anchor)event.getSource();
+							fireFacetSelectionEvent(src.getText(),false);
+						}
+						  
+					  });
 				}
 			}
 		}
